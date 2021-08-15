21b066f38701ef31a570656ebf909b8844249027
Gustavo Gomes Balbino <gustavo.gbalbino@hotmail.com>
2021-08-14 21:15:10 -0300

    exercicios

--- Exercices/ExercíciosPropostos01.py
+++ Exercices/ExercíciosPropostos01.py
@@ -11,3 +11,47 @@ C) Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 le
 menos, escreva: 'Seu nome é curto'; se tiver entre 5 e 6 letras, escreva: "Seu nome é normal"; maior que 6 letras
 escreva: 'Seu nome é grande'.
 '''
+
+# RESPOSTAS:
+# A)
+
+num = input('Digite um número inteiro: ')
+
+if num.isdigit():
+    num = int(num)
+    if num % 2 == 0:
+        print(f'{num} é par')
+    else:
+        print(f'{num} é ímpar.')
+else:
+    print('Isso não é um número inteiro.')
+
+
+# B)
+
+hora = input('Digite o horário. (00 - 23h) ')
+
+if hora.isdigit():
+    hora = int(hora)
+    if hora < 0 or hora > 23:
+        print('Por favor digite um horário entre 00 e 23h.')
+    else:
+        if hora <= 11:
+            print(f'{hora}h - BOM DIA!')
+        elif hora <= 17:
+            print(f'{hora}h - BOA TARDE!')
+        else:
+            print(f'{hora}h - BOA NOITE!')
+
+
+# C)
+
+nome = input('Digite seu nome de usuário: ')
+tamanho = len(nome)
+
+if tamanho <= 4:
+    print(f'Seu nome é curto, {tamanho} letras totais.')
+elif tamanho == 5 or tamanho == 6:
+    print(f'Seu nome é normal, {tamanho} letras totais.')
+else:
+    print(f'Seu nome é grande, {tamanho} letras totais.')
