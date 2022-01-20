bbc5bbd88789547d2817dfaac4bb44b90a26ae1f
Gus <gustavo.gbalbino@hotmail.com>
2022-01-20 10:24:34 -0300

    01/20/2022

--- Procedural Programming/Notes05.py
+++ Procedural Programming/Notes05.py
@@ -84,7 +84,7 @@ for group in product(person, repeat=2):
 
 #Groupby
 
-students =
+students = [
     {'Name': 'Gus', 'Grade': 'A+'}
     {'Name': 'Yas', 'Grade': 'B-'}
     {'Name': 'Bea', 'Grade': 'C+'}
@@ -94,8 +94,7 @@ students =
     {'Name': 'Maria', 'Grade': 'B+'}
     {'Name': 'Vic', 'Grade': 'C-'}
     {'Name': 'João', 'Grade': 'F'}
-    {'Name': 'Nath', 'Grade': 'D+'}
-
+    {'Name': 'Nath', 'Grade': 'D+'} ]
 
 students.sort(key=lambda item: item('Grade'))
 
